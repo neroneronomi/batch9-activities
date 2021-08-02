2c78904cf73f97a9a75bf5829ae34f93a413ff1c
neroneronomi <oliver_nero11@icloud.com>
2021-08-03 05:26:36 +0800

    lesson_1: finished

---
+++ ruby_activities/lesson_1/hash_get_value.rb
@@ -0,0 +1,10 @@
+#Suppose you have a hash `h = {a:1, b:2, c:3, d:4}`. 
+#Get the value of key `:b`. Add to this hash the key:value pair `{e:5}`
+
+#Get value
+h = {a:1, b:2, c:3, d:4}
+puts h[:b]
+
+#Add hash
+h[:e] = 5
+puts h
---
+++ ruby_activities/lesson_1/iterate_array.rb
@@ -0,0 +1,4 @@
+#Use the `each` method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], and print out each value.
+
+array_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array_numbers.each { |e| puts e }
