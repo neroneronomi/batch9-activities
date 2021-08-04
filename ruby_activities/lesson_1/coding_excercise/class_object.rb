d28eba3353999845363f01ef78d0749d4395192c
neroneronomi <oliver_nero11@icloud.com>
2021-08-04 20:11:10 +0800

    code alignment

--- ruby_activities/lesson_1/coding_excercise/class_object.rb
+++ ruby_activities/lesson_1/coding_excercise/class_object.rb
@@ -4,11 +4,9 @@ class Pet
       @animal = animal
       @color = color
   end
-
   def details
       puts "My pet name is #{@name}! He is a #{@animal}, and his color is #{@color}."
   end
 end
-
 pet_1 = Pet.new("Yam", "Cat", "White")
-pet_1.details
+pet_1.detailsgi
