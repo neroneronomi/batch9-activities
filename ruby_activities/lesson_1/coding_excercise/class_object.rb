39013337ce7c96adfbdcef3df13c5ab5a5476e8b
neroneronomi <oliver_nero11@icloud.com>
2021-08-04 20:12:17 +0800

    code alignment

--- ruby_activities/lesson_1/coding_excercise/class_object.rb
+++ ruby_activities/lesson_1/coding_excercise/class_object.rb
@@ -1,11 +1,11 @@
 class Pet
   def initialize(name, animal, color)
-      @name = name
-      @animal = animal
-      @color = color
+    @name = name
+    @animal = animal
+    @color = color
   end
   def details
-      puts "My pet name is #{@name}! He is a #{@animal}, and his color is #{@color}."
+    puts "My pet name is #{@name}! He is a #{@animal}, and his color is #{@color}."
   end
 end
 pet_1 = Pet.new("Yam", "Cat", "White")
