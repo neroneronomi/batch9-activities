6877143ecc537a6794aaa302984bda99675b4ea0
neroneronomi <oliver_nero11@icloud.com>
2021-08-09 19:40:29 +0800

    keep_hydrated

---
+++ ruby_activities/coding_excercise/keep_hydrated.rb
@@ -0,0 +1,8 @@
+#Codewars link: https://www.codewars.com/kata/582cb0224e56e068d800003c/train/ruby
+
+def litres(time)
+  return (time * 0.5).floor
+end
+
+#.floor - Round off the number
+#.to_i - Takes the number and convert to integer, Takes the whole number if has decimal
