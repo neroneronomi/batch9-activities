f9997508635077567e2e76085849c811cd50bce7
neroneronomi <oliver_nero11@icloud.com>
2021-09-16 19:39:20 +0800

    iq_test

--- ruby_activities/coding_excercise/it_test.rb
+++ ruby_activities/coding_excercise/it_test.rb
@@ -1,6 +1,14 @@
 #Codewars link: https://www.codewars.com/kata/552c028c030765286c00007d/train/ruby
 def iq_test(numbers)
-  numbers = numbers.split.map(&:to_i)
-  even, odd = numbers.partition { |n| n.even? }
-  even.count == 1 ? numbers.index(even.join.to_i) + 1 : numbers.index(odd.join.to_i) + 1
+  numbers = numbers.split().map{|num| num.to_i}
+  even_count = numbers.select {|num| num.even?}.count
+  odd_count = numbers.select {|num| num.odd?}.count
+
+  numbers.each_with_index do |num, i|
+    if even_count > odd_count
+      return i + 1 if num.odd?
+    else
+      return i + 1 if num.even?
+    end
+  end
 end
